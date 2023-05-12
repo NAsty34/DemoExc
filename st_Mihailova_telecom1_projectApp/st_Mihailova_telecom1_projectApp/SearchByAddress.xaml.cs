b929a0713487107f24b08abd6e0510590e7e7214
NAsty34 <99412848+NAsty34@users.noreply.github.com>
2023-05-13 01:47:33 +0300

    guestprot

---
+++ st_Mihailova_telecom1_projectApp/st_Mihailova_telecom1_projectApp/SearchByAddress.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace st_Mihailova_telecom1_projectApp
+{
+    /// <summary>
+    /// Логика взаимодействия для SearchByAddress.xaml
+    /// </summary>
+    public partial class SearchByAddress : Window
+    {
+        public SearchByAddress()
+        {
+            InitializeComponent();
+        }
+    }
+}
